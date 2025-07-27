28d38fa64bfe15d5b960f2dc4ac2cd2f722cf9bd
owhonda-moses <owhondamoses7@gmail.com>
2025-07-27 01:26:58 +0000

    Fixed prefect agent in yaml

---
+++ src/iot_anomaly/agent_service.py
@@ -0,0 +1,18 @@
+import subprocess
+import os
+from flask import Flask, jsonify
+
+# start prefect agent as a background process to poll for new flow runs
+subprocess.Popen(["prefect", "agent", "start", "-q", "default"])
+
+
+app = Flask(__name__)
+
+@app.route("/health", methods=["GET"])
+def health():
+    """A simple health check endpoint."""
+    return jsonify({"status": "ok"}), 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
