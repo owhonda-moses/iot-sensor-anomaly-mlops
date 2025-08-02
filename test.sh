801557861db6b8ff3b28daa7392e8cc47b97a36e
owhonda-moses <owhondamoses7@gmail.com>
2025-08-02 17:26:45 +0100

    Updated authentication for prediction service

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+curl -X POST "https://prediction-service-243279652112.europe-west2.run.app/predict" \
+-H "Content-Type: application/json" \
+-d '{
+      "data": [
+        {
+          "device": "b8:27:eb:bf:9d:51",
+          "ts": 1593590400,
+          "co": 0.004,
+          "humidity": 75.4,
+          "light": true,
+          "lpg": 0.007,
+          "motion": false,
+          "smoke": 0.019,
+          "temp": 21.4
+        }
+      ]
+    }'
